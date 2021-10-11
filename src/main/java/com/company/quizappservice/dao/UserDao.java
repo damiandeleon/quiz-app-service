b4417a7b05844c224e53b422160e84c3ae244a5a
Tom Capers <tompcapers@gmail.com>
2021-10-11 15:20:05 -0700

    renamed to ScoreDao and updated annotations

--- src/main/java/com/company/quizappservice/dao/UserDao.java
+++ src/main/java/com/company/quizappservice/dao/UserDao.java
@@ -2,18 +2,12 @@ package com.company.quizappservice.dao;
 
 import com.company.quizappservice.dto.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserDao extends JpaRepository<User, Integer> {
 
-    public User addUser(User user);
 
-    public User getUser(int id);
-
-    public List<User> getAllUsers();
-
-    public void updateUser(User user);
-
-    public void deleteUser(int id);
 }
