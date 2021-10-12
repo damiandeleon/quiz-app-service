5080e2ab600ed00ce2c5147a00cffa592634fc23
Damian De Leon <damianmdeleon@gmail.com>
2021-10-12 09:55:38 -0500

    updated pom.xl file with Junit and added controller test folder

---
+++ src/test/java/com/company/quizappservice/controller/QuizAppServiceControllerTest.java
@@ -0,0 +1,10 @@
+package com.company.quizappservice.controller;
+
+import static org.junit.Assert.*;
+
+public class QuizAppServiceControllerTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+}
